bb9c26749dd868dc0e567e24359bbfafdfb12c3a
tekbyts <tekbyts@gmail.com>
2023-10-23 15:48:09 -0400

    feat(fix): Changes didn't apply. Fix it.

--- projects/jokes/src/app/jokes/jokes-list/Joke.ts
+++ projects/jokes/src/app/jokes/jokes-list/Joke.ts
@@ -1,4 +1,5 @@
 export interface Joke {
   setup: string;
   punchline: string;
+  hide: boolean;
 }
--- projects/jokes/src/app/jokes/jokes-list/jokes-list.component.ts
+++ projects/jokes/src/app/jokes/jokes-list/jokes-list.component.ts
@@ -14,14 +14,17 @@ export class JokesListComponent {
       {
         setup: 'What did the cheese say when it looked in the mirror?',
         punchline: 'Hello-Me (Halloumi)',
+        hide: true,
       },
       {
         setup: 'What kind of cheese do you use to disguise a small horse?',
         punchline: 'Mask-a-pony (Mascarpone)',
+        hide: true,
       },
       {
         setup: 'A kid threw a lump of cheddar at me',
         punchline: 'I thought ‘That’s not very mature’',
+        hide: true,
       },
     ];
   }
